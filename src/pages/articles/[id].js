0f37f24cc4c2ce81acbda18b81ab6e040753429f
duong-vo <vodh2@miamioh.edu>
2023-07-28 20:19:28 +0700

    add a sticky app bar for each article

--- src/pages/articles/[id].js
+++ src/pages/articles/[id].js
@@ -1,11 +1,32 @@
+import React from 'react';
 import { useRouter } from 'next/router';
+import AppBar from '@mui/material/AppBar';
+import Grid from '@mui/material/Grid';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import SearchBar from '../../components/SearchBar';
 
-export default function Article() {
+const Article = () => {
   const router = useRouter();
   const { id } = router.query;
   return (
     <div>
-      Hi {id}
+      <AppBar position="sticky">
+        <Toolbar>
+          <Grid container spacing={2}>
+            <Grid item xs={4}>
+              <Typography>
+                Test
+              </Typography>
+            </Grid>
+            <Grid item xs={8}>
+              <SearchBar />
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
+
+export default Article;
